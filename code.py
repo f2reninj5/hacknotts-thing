1f4a92baeb9a70984f6f84dd29787b1102aa8411
fn <64966099+f2reninj5@users.noreply.github.com>
2025-10-25 15:44:04 +0100

    Send data serially

--- code.py
+++ code.py
@@ -1,9 +1,16 @@
-from inputs import buttons, sliders
+import struct
+
+from inputs import buttons_to_bytes, sliders_to_bytes
+import usb_cdc
 import time
 
+usb_cdc.enable(console=True, data=True)
+data = usb_cdc.data
+
 while True:
-    for i, button in enumerate(buttons):
-        print(f"Button {i} value: {button.value}")
-    for i, slider in enumerate(sliders):
-        print(f"Slider {i} value: {slider.value}")
-    time.sleep(1)
+    button_bytes = buttons_to_bytes()
+    slider_bytes = sliders_to_bytes()
+    struct_format = "<" + "B" * len(button_bytes) + "H" * len(slider_bytes)
+    payload = struct.pack(struct_format, *button_bytes, *slider_bytes)
+    usb_cdc.data.write(payload)
+    time.sleep(0.1)
--- inputs.py
+++ inputs.py
@@ -23,3 +23,19 @@ sliders = [
         board.GP28_A2,
     ]
 ]
+
+
+def sliders_to_bytes():
+    return [slider.value for slider in sliders]
+
+
+def buttons_to_bytes():
+    button_bytes = []
+    for i in range(len(buttons) // 8):
+        byte = 0
+        buttons_chunk = buttons[i * 8 : (i + 1) * 8]
+        for button in buttons_chunk:
+            if button.value:
+                byte |= 1 << (i * 8)
+                button_bytes.append(byte)
+    return button_bytes
