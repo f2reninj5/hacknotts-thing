7e229fa4668da3a9d74f6a8cbcd10b0ebd38397a
fn <64966099+f2reninj5@users.noreply.github.com>
2025-10-25 15:57:32 +0100

    Write local serial reader

--- inputs.py
+++ inputs.py
@@ -31,7 +31,7 @@ def sliders_to_bytes():
 
 def buttons_to_bytes():
     button_bytes = []
-    for i in range(len(buttons) // 8):
+    for i in range((len(buttons) // 8) + 1):
         byte = 0
         buttons_chunk = buttons[i * 8 : (i + 1) * 8]
         for button in buttons_chunk:
---
+++ local.py
@@ -0,0 +1,26 @@
+import serial
+import serial.tools.list_ports
+import struct
+
+
+def find_pico():
+    for port in serial.tools.list_ports.comports():
+        if port.vid == 0x2E8A:
+            return port.device
+    return None
+
+
+NUM_BUTTONS = 3
+NUM_SLIDERS = 3
+NUM_BUTTON_BYTES = (NUM_BUTTONS // 8) + 1
+NUM_SLIDER_BYTES = NUM_SLIDERS * 2
+STRUCT_SIZE = NUM_BUTTON_BYTES + NUM_SLIDER_BYTES
+struct_format = "<" + "B" * NUM_BUTTON_BYTES + "H" * NUM_SLIDER_BYTES
+
+if __name__ == "__main__":
+    ser = serial.Serial(find_pico(), 115200, timeout=1)
+
+    while True:
+        data = ser.read(STRUCT_SIZE)
+        values = struct.unpack(struct_format, data)
+        print(values)
