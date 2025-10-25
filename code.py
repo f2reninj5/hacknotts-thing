c71d5a2568affb256481ae1308a7a96fc5765a1c
fn <64966099+f2reninj5@users.noreply.github.com>
2025-10-25 14:47:17 +0100

    Add simple slider logic

--- code.py
+++ code.py
@@ -1,18 +1,33 @@
 import board
 import digitalio
+import analogio
 import time
 
-buttons = [digitalio.DigitalInOut(pin) for pin in [
-    board.GP16,
-    board.GP17,
-    board.GP18,
-]]
+buttons = [
+    digitalio.DigitalInOut(pin)
+    for pin in [
+        board.GP16,
+        board.GP17,
+        board.GP18,
+    ]
+]
 
 for button in buttons:
     button.direction = digitalio.Direction.INPUT
     button.pull = digitalio.Pull.DOWN
 
+sliders = [
+    analogio.AnalogIn(pin)
+    for pin in [
+        board.GP26_A0,
+        board.GP27_A1,
+        board.GP28_A2,
+    ]
+]
+
 while True:
     for i, button in enumerate(buttons):
         print(f"Button {i} value: {button.value}")
-    time.sleep(0.1)
+    for i, slider in enumerate(sliders):
+        print(f"Slider {i} value: {slider.value}")
+    time.sleep(1)
