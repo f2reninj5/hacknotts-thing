4c29aafe461ca1d623cca849b2e3b4ff188b03d6
fn <64966099+f2reninj5@users.noreply.github.com>
2025-10-25 14:44:07 +0100

    Add digital button logic

--- code.py
+++ code.py
@@ -1,8 +1,18 @@
 import board
+import digitalio
+import time
 
-buttons = [
+buttons = [digitalio.DigitalInOut(pin) for pin in [
     board.GP16,
     board.GP17,
     board.GP18,
-]
+]]
 
+for button in buttons:
+    button.direction = digitalio.Direction.INPUT
+    button.pull = digitalio.Pull.DOWN
+
+while True:
+    for i, button in enumerate(buttons):
+        print(f"Button {i} value: {button.value}")
+    time.sleep(0.1)
